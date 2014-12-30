8f4c619a49c493274d1160fd4268220fe689057c
Robert Massa <robertmassa@gmail.com>
2014-12-30 13:38:02 +0100

    Add Newtonsoft.JSON as NuGet dependency.

--- PostcodeNL.Test/Program.cs
+++ PostcodeNL.Test/Program.cs
@@ -4,6 +4,7 @@ namespace PostcodeNL.Test {
     class Program {
         static void Main(string[] args) {
             var api = new AddressApi(apiKey: "<your key>", apiSecret: "<your secret>");
+
             var result = api.LookupAsync("2012ES", 30).Result;
 
             Console.WriteLine(result);
